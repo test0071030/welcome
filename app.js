58eb30ad1156933d893552c7ab4341bb86d71ab7
test0071030 <47777484+test0071030@users.noreply.github.com>
2019-04-29 11:19:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
 }
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
